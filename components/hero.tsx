4280fec7527a71cc7d6a1085cfa94d810f5a1810
Matias Verdier <verdier.matias@gmail.com>
2020-09-14 01:33:03 -0300

    Change hero text

--- components/hero.tsx
+++ components/hero.tsx
@@ -74,9 +74,9 @@ export default function Hero() {
         <main className="mt-10 mx-auto max-w-screen-xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 xl:mt-28">
           <div className="text-center">
             <h2 className="text-4xl tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-              #freeCodeCampMVD: Hola Mundo!
+              FreeCodeCamp
               <br className="xl:hidden" />
-              <span className="text-indigo-600">Página oficial</span>
+              <span className="text-indigo-600">Montevideo</span>
             </h2>
             <p className="mt-3 max-w-md mx-auto text-base text-gray-500 sm:text-lg md:mt-5 md:text-xl md:max-w-3xl">
               Bienvenidos a la comunidad freeCodeCamp de Montevideo!!
