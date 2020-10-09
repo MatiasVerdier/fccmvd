d1c9416095f14c8188d65fd2d2d092249ac859fc
Matias Verdier <verdier.matias@gmail.com>
2020-10-08 23:39:04 -0300

    Active state for nav links

--- components/navigation-link.tsx
+++ components/navigation-link.tsx
@@ -1,13 +1,20 @@
 import Link from 'next/link';
 import cn from 'classnames';
+import { useRouter } from 'next/router';
 
-export default function NavigationLink({ href, isMobile = false, children }) {
+export default function NavigationLink({ href, isMobile = false, children, ...props }) {
+  const { asPath } = useRouter()
+  const isActive = asPath === href || asPath === props.as
+
   return (
-    <Link href={href}>
+    <Link href={href} {...props}>
       <a
         className={cn({
-          'ml-4 px-3 py-2 rounded-md text-sm font-medium leading-5 text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out': !isMobile,
-          'mt-1 block px-3 py-2 rounded-md text-base font-medium text-gray-300 hover:text-white hover:bg-gray-700 focus:outline-none focus:text-white focus:bg-gray-700 transition duration-150 ease-in-out': isMobile,
+          'px-3 py-2 rounded-md font-medium hover:text-white hover:bg-gray-700 focus:outline-none transition duration-150 ease-in-out': true,
+          'ml-4 text-sm leading-5 ': !isMobile,
+          'mt-1 block text-base': isMobile,
+          'text-gray-300': !isActive,
+          'text-white bg-gray-900': isActive,
         })}
       >
         {children}
