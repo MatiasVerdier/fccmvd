f0338cfd9280601d2213afa70e6282220cfd9bbf
Matias Verdier <verdier.matias@gmail.com>
2020-12-19 03:21:33 -0300

    Add link in the featured section to view all posts

--- components/blog/featured.tsx
+++ components/blog/featured.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import ArticleCard from './article-card';
 
 export default function Featured({ posts }) {
@@ -19,6 +20,14 @@ export default function Featured({ posts }) {
           {posts.map((post) => (
             <ArticleCard key={post.title} {...post} />
           ))}
+        </div>
+
+        <div className="mt-6 flex justify-center">
+          <Link href="/blog">
+            <a className="inline-flex items-center px-6 py-3 border border-gray-300 shadow-sm text-base font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              Ver todos
+            </a>
+          </Link>
         </div>
       </div>
     </div>
