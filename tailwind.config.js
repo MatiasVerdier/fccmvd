6d034cf764e0790ebaffd7870a697e596d37188e
Matias Verdier <verdier.matias@gmail.com>
2021-12-11 16:13:43 -0300

    Upgrate tailwind and plugins

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,5 +11,6 @@ module.exports = {
     require('@tailwindcss/aspect-ratio'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('@tailwindcss/line-clamp'),
   ],
 };
