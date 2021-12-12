f1f44ff1b26737d1d35f52abbe1da303d9b01da2
Matias Verdier <verdier.matias@gmail.com>
2021-12-12 17:05:44 -0300

    Update post

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./pages/**/*.tsx', './components/**/*.tsx'],
+  purge: ['./pages/**/*.tsx', './components/**/*.tsx', './content/**/*.mdx'],
   theme: {
     extend: {},
   },
